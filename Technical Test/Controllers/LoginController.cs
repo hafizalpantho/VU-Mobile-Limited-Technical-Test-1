0717fb5caf75a89514531a21b31811e6edda841e
hafizalpantho <hafizalpantho@gmail.com>
2021-04-06 11:03:57 +0600

    first commit

---
+++ Technical Test/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Technical_Test.Models;
+
+namespace Technical_Test.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        LoginContext context = new LoginContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Login log)
+        {
+
+            var user = context.Logins.Where(x => x.UserName == log.UserName && x.Password == log.Password).Count();
+            if(user >0)
+            {
+                return RedirectToAction("Welcome");
+
+            }
+            else
+            {
+                ViewBag.Message = "Sorry wrong information given";
+                return View();
+            }
+            
+            
+        }
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+    }
+}
